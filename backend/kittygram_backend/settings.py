375cd535a5ab8162cf39f48caba17f573c431736
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-07-02 22:40:58 +0000

    review1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,15 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+load_dotenv()
+SECRET_KEY = os.getevn('TOKEN', 'SECRET')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.252.140.107', '127.0.0.1', 'localhost', 'prettykittygram.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +92,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
